8017297863d2b0c9b5ae6e58d81c787cb55bd1a6
nelliott <nelliott@skywardapps.com>
2014-07-17 09:02:47 -0400

    Initial Commit

---
+++ KnockoutOC/KnockoutOC.h
@@ -0,0 +1,13 @@
+//
+//  KnockoutOC.h
+//  KnockoutOC
+//
+//  Created by Nicholas Elliott on 7/17/14.
+//  Copyright (c) 2014 Skyward App Company, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KnockoutOC : NSObject
+
+@end
---
+++ KnockoutOC/KnockoutOC.m
@@ -0,0 +1,13 @@
+//
+//  KnockoutOC.m
+//  KnockoutOC
+//
+//  Created by Nicholas Elliott on 7/17/14.
+//  Copyright (c) 2014 Skyward App Company, LLC. All rights reserved.
+//
+
+#import "KnockoutOC.h"
+
+@implementation KnockoutOC
+
+@end

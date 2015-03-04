aaa8925dacc96fa13d88a73f7ab3a3f1be7c1ac8
nelliott <nelliott@skywardapps.com>
2015-03-04 14:59:30 -0500

    Working Lua bridge!

--- android/kenshointernal/src/main/jni/luabridge.cpp
+++ android/kenshointernal/src/main/jni/luabridge.cpp
@@ -68,176 +68,176 @@ public:
     char returnType;
 };
 
-int pushValue(lua_State* L, jobject value)
-{
-    return 1;
+extern "C" {
+    int pushValue(lua_State* L, jobject value)
+    {
+        return 1;
+    }
+
+    jobject popValue(lua_State* L, int stackLevel, LuaWrapper* wrapper)
+    {
+        // Now figure out the datatype of the assignment and get the data out
+        if(lua_isboolean(L, stackLevel))
+        {
+            bool value = lua_toboolean(L, stackLevel);
+            //return wrapper->boolToBoolean(value);
+        }
+        else if(lua_isnumber(L, stackLevel))
+        {
+            double value = lua_tonumber(L, stackLevel);
+            return wrapper->doubleToNumber(value);
+        }
+        else if(lua_isstring(L, stackLevel))
+        {
+            const char* tempString = lua_tostring(L, stackLevel);
+            //return wrapper->cstrToJString(tempString);
+        }
+        return NULL;
+    }
+
+    int lookupKey(lua_State* L)
+    {
+        // Stack is [table, key]
+        if(!lua_isstring(L, -1))
+        {
+            return 0;
+        }
+
+        if(!lua_istable(L, -2))
+        {
+            return 0;
+        }
+
+        // Get the pointer to the context object
+        lua_getfield(L, -2, "__self");
+        jobject target = (jobject)lua_touserdata(L, -1);
+        if(target == NULL)
+        {
+            return 0;
+        }
+
+        const char* nameRequest = lua_tostring(L, -2);
+        if(nameRequest == NULL)
+        {
+            return 0;
+        }
+
+        // Now we need to potentially do a reflection map of this object
+        // Now check if there is actually a property as requested
+        // If so, call it and get the result
+        jobject value = NULL;
+        return pushValue(L, value);
+    }
+
+    int lookupNewKey(lua_State* L)
+    {
+        // Stack is [table, key, value]
+
+         // Verify that the key is a string
+         if(!lua_isstring(L, -2))
+         {
+             return 0;
+         }
+
+         // Make sure that the context is a table.  This should actually be the global table.
+         if(!lua_istable(L, -3))
+         {
+             return 0;
+         }
+
+         // Make sure the key is a valid string too!
+         const char* nameRequest = lua_tostring(L, -2);
+         if(nameRequest == NULL)
+         {
+             return 0;
+         }
+
+         // Get the pointer to the context table (the global table here)
+         // And then get its __wrapper object, which points to the owning KenshoLuaWrapper.
+         lua_getfield(L, -3, "__wrapper");
+         LuaWrapper* target = (LuaWrapper*)lua_touserdata(L, -1);
+         lua_pop(L, 1);
+         if(target == NULL)
+         {
+             return 0;
+         }
+
+         jobject value = popValue(L, -1, target);
+
+         // Ok, we save this value to the 'parameters' object
+         target->setResultParameter(nameRequest, value);
+
+         return 1;
+
+    }
+
+    JNIEXPORT jobject JNICALL Java_com_skywardapps_kensho_LuaWrapper_luaEvaluate(JNIEnv* env, jobject sender, jobject context, jstring code)
+    {
+        if(luaWrapper == NULL)
+        {
+            luaWrapper = new LuaWrapper(env, sender); // \todo worry about the lifetime of this
+        }
+
+        const char* cstrLuaCode = env->GetStringUTFChars(code, NULL);
+
+        lua_State* L = (lua_State *) luaL_newstate();
+        luaL_openlibs(L);
+
+        lua_pushlightuserdata(L, context);
+        lua_setglobal(L, "__self");
+
+        lua_pushlightuserdata(L, luaWrapper);
+        lua_setglobal(L, "__wrapper");
+
+        //-- get global environment table from registry
+        lua_pushglobaltable(L);
+
+        //-- create table containing the hook details
+        lua_newtable(L);
+        lua_pushstring(L, "__index");
+        lua_pushcfunction(L, lookupKey);
+        lua_settable(L, -3);
+
+        lua_pushstring(L, "__newindex");
+        lua_pushcfunction(L, lookupNewKey);
+        lua_settable(L, -3);
+
+        //-- set global index callback hook
+        lua_setmetatable(L, -2);
+
+        //-- remove the global environment table from the stack
+        lua_pop(L, 1);
+
+        /* run the test script */
+        luaL_loadstring(L, cstrLuaCode);
+        int resultCode = lua_pcall(L, 0, 1, 0);
+
+        /*
+         LUA_ERRRUN: a runtime error.
+         LUA_ERRMEM: memory allocation error. For such errors, Lua does not call the error handler function.
+         LUA_ERRERR: error while running the error handler function.
+        */
+        switch(resultCode)
+        {
+            case 0:
+                // The success case here
+                break;
+            case LUA_ERRRUN:
+                __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Runtime error: %s\n%s", lua_tostring(L, -1), cstrLuaCode);
+                break;
+            case LUA_ERRMEM:
+                __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Memory allocation error: %s\n%s", lua_tostring(L, -1), cstrLuaCode);
+                break;
+            case LUA_ERRSYNTAX:
+                __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Syntax error: %s\n%s", lua_tostring(L, -1), cstrLuaCode);
+                break;
+            case LUA_ERRERR:
+                __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Error handler error: %s\n%S", lua_tostring(L, -1), cstrLuaCode);
+                break;
+        }
+
+        env->ReleaseStringUTFChars(code, cstrLuaCode);
+
+        return NULL;
+    }
 }
-
-jobject popValue(lua_State* L, int stackLevel, LuaWrapper* wrapper)
-{
-    // Now figure out the datatype of the assignment and get the data out
-    if(lua_isboolean(L, stackLevel))
-    {
-        double value = lua_toboolean(L, stackLevel);
-        return wrapper->doubleToNumber(value);
-    }
-    /*else if(lua_isnumber(L, stackLevel))
-    {
-        return  @(lua_tonumber(L, stackLevel));
-    }
-    else if(lua_isstring(L, stackLevel))
-    {
-        const char* tempString = lua_tostring(L, stackLevel);
-        return [NSString stringWithUTF8String:tempString];
-    }*/
-    return NULL;
-}
-
-int lookupKey(lua_State* L)
-{
-    // Stack is [table, key]
-    if(!lua_isstring(L, -1))
-    {
-        return 0;
-    }
-
-    if(!lua_istable(L, -2))
-    {
-        return 0;
-    }
-
-    // Get the pointer to the context object
-    lua_getfield(L, -2, "__self");
-    jobject target = (jobject)lua_touserdata(L, -1);
-    if(target == NULL)
-    {
-        return 0;
-    }
-
-    const char* nameRequest = lua_tostring(L, -2);
-    if(nameRequest == NULL)
-    {
-        return 0;
-    }
-
-    // Now we need to potentially do a reflection map of this object
-    // Now check if there is actually a property as requested
-    // If so, call it and get the result
-    jobject value = NULL;
-    return pushValue(L, value);
-}
-
-int lookupNewKey(lua_State* L)
-{
-    // Stack is [table, key, value]
-
-     // Verify that the key is a string
-     if(!lua_isstring(L, -2))
-     {
-         return 0;
-     }
-
-     // Make sure that the context is a table.  This should actually be the global table.
-     if(!lua_istable(L, -3))
-     {
-         return 0;
-     }
-
-     // Make sure the key is a valid string too!
-     const char* nameRequest = lua_tostring(L, -2);
-     if(nameRequest == NULL)
-     {
-         return 0;
-     }
-
-     // Get the pointer to the context table (the global table here)
-     // And then get its __wrapper object, which points to the owning KenshoLuaWrapper.
-     lua_getfield(L, -3, "__wrapper");
-     LuaWrapper* target = (LuaWrapper*)lua_touserdata(L, -1);
-     lua_pop(L, 1);
-     if(target == NULL)
-     {
-         return 0;
-     }
-
-     jobject value = popValue(L, -1, target);
-
-     // Ok, we save this value to the 'parameters' object
-     target->setResultParameter(nameRequest, value);
-
-     return 1;
-
-}
-
-extern "C"{
-JNIEXPORT jobject JNICALL Java_com_skywardapps_kensho_LuaWrapper_luaEvaluate(JNIEnv* env, jobject sender, jobject context, jstring code)
-{
-    if(luaWrapper == NULL)
-    {
-        luaWrapper = new LuaWrapper(env, sender); // \todo worry about the lifetime of this
-    }
-
-    const char* cstrLuaCode = env->GetStringUTFChars(code, NULL);
-
-    lua_State* L = (lua_State *) luaL_newstate();
-    luaL_openlibs(L);
-
-    lua_pushlightuserdata(L, context);
-    lua_setglobal(L, "__self");
-
-    lua_pushlightuserdata(L, luaWrapper);
-    lua_setglobal(L, "__wrapper");
-
-    //-- get global environment table from registry
-    lua_pushglobaltable(L);
-
-    //-- create table containing the hook details
-    lua_newtable(L);
-    lua_pushstring(L, "__index");
-    lua_pushcfunction(L, lookupKey);
-    lua_settable(L, -3);
-
-    lua_pushstring(L, "__newindex");
-    lua_pushcfunction(L, lookupNewKey);
-    lua_settable(L, -3);
-
-    //-- set global index callback hook
-    lua_setmetatable(L, -2);
-
-    //-- remove the global environment table from the stack
-    lua_pop(L, 1);
-
-    /* run the test script */
-    luaL_loadstring(L, cstrLuaCode);
-    int resultCode = lua_pcall(L, 0, 1, 0);
-
-    /*
-     LUA_ERRRUN: a runtime error.
-     LUA_ERRMEM: memory allocation error. For such errors, Lua does not call the error handler function.
-     LUA_ERRERR: error while running the error handler function.
-    */
-    switch(resultCode)
-    {
-        case 0:
-            // The success case here
-            break;
-        case LUA_ERRRUN:
-            __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Runtime error: %s\n%s", lua_tostring(L, -1), cstrLuaCode);
-            break;
-        case LUA_ERRMEM:
-            __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Memory allocation error: %s\n%s", lua_tostring(L, -1), cstrLuaCode);
-            break;
-        case LUA_ERRSYNTAX:
-            __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Syntax error: %s\n%s", lua_tostring(L, -1), cstrLuaCode);
-            break;
-        case LUA_ERRERR:
-            __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Error handler error: %s\n%S", lua_tostring(L, -1), cstrLuaCode);
-            break;
-    }
-
-    env->ReleaseStringUTFChars(code, cstrLuaCode);
-
-    return NULL;
-}
-}
-
